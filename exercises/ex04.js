736c5ebbe18b774811ceb0f03c9e3ef4f00658d7
LynaHwang <lynahwang3@gmail.com>
2026-04-24 20:39:33 -0700

    cows
    new lab

---
+++ exercises/ex04.js
@@ -0,0 +1,18 @@
+let count=0;
+let colors=["Orchid", "Coral", "HotPink", "Plum"];
+
+$("#needy-button").click( function(){
+    
+    
+
+    $("#needy-button").html("you clicked me " + count + " and your favourite color today is: " + colors[count] );
+
+    $("#needy-button").css( "background-color", colors[count]);  
+
+    count = count +1;
+
+    $("html").css( "background-color", colors[count]);
+
+    
+
+});
