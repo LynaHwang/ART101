7c699c48a497dace8d402598f687f9b4d34baf7d
LynaHwang <lynahwang3@gmail.com>
2026-05-25 21:25:38 -0700

    cowcow

--- environ/env03.js
+++ environ/env03.js
@@ -17,23 +17,21 @@ let cowbutton = {
     species: "plant",
     moods: ["hmm", "yum", "yuck", "full"]
 };
-
 $("#cowbutton").click(function () {
-
-    count = count + 1;
+count = count + 1;
     let arrayPosition = count - 1;
     let currentMood = cowbutton.moods[arrayPosition];
     let message = "<p>You clicked me " + count + " times.</p>";
     message = message + "<p>My current mood is " + currentMood + ".</p>";
     message = message + "<p>My name is " + cowbutton.name + ".</p>";
-    message = message + "<p>I am an " + cowbutton.species + ".</p>";
-   
-
+    message = message + "<p>I am an " + cowbutton.species + ".</p>";count = count + 1;
     $("#output").html(message);
 
     console.log(count);
     console.log(arrayPosition);
     console.log(currentMood);
+
+
 
 function askNumber(whatNumber) {
     let userNumber = prompt("Rate the Cows 1/10?");
@@ -52,8 +50,6 @@ $("#good-button").click(function () {
 
 
 });
-
-
 
 
 
--- garden/creatures.js
+++ garden/creatures.js
@@ -1,28 +1,93 @@
-// add creature to the garden
+
+// store all creatures in an array
+let allCreatures=[];
+
+// functions
+// function to grab data from the form
+function getCreatureFromForm(){
+  
+  const freshCreature={
+        name: $("#crName").val(),
+        color: $("#crColor").val(),
+        eyesNum: $("#crEyesNum").val()
+    };
+
+return freshCreature;
+};
+
+// random creature
+
+function randomizeCreature () { 
+ 
+ const randomCreature= {
+        name: "random",
+        color: "lime",
+        eyesNum: 3
+    };
+return randomCreature;
+
+}
+
+// the checks function
+function isCreatureValid(creature) {
+  if (creature.name === "" ) return false;
+  if (creature.name.length >12) return false;
+  if ( isNaN(creature.eyesNum) || creature.eyesNum >5 ) return false;
+  return true;
+}
+
+// functions add creature to the page
+function renderCreature(creature) {
+
+    let crEyesHtml="";
+    for (let i = 0; i < creature.eyesNum; i++) {
+        crEyesHtml=crEyesHtml+ "<div class='eye'>.</div>";
+    }
+
+    const html=`
+        <div class="creature">
+            <div class="creature-body" style="background-color: ${creature.color}"> ${crEyesHtml} </div>
+            <div class="creature-info">${creature.name}</div>
+        </div>
+    `;
+
+    return html;
+}
+
+// append creature to the page
+function addCreatureToDOM(creature) {
+    const html=renderCreature(creature);
+    $("#creature-list").append(html);
+}
+
+// the main brain
 $("#crAdd").click( function(){
 
-    // grab the value from the text input and assign it to a variable crName
-    let crName=$("#crName").val();
-    let crColor=$("#crColor").val();
-    let crEyesNum=$("#crEyesNum").val();
-
-    // test in console
-    console.log(crName);  
-    console.log(crColor); 
-    console.log(crEyesNum); 
-    
-
-    if( crName.length > 2) { 
-             $("#creature-list").append(`
-            <div class="creature">
-                <div class="creature-body"></div>
-                <div class="creature-info">${crName}</div>
-            </div>
-        `);+crName+crColor+crEyesNum+"</div>");
+    let newCreature;
+    // choose the way /random or manual
+    // if checked go random mode
+    if ( $("#crRandom").is(":checked") ) {
+       newCreature = randomizeCreature();
+       console.log("random way");
     }
-   
+    // if not checked go manual mode
+    else { newCreature = getCreatureFromForm(); console.log("manual way"); }
 
-     $("#crName").val(""); // write the value
-    // $("#crName").val(); // retrieve the value
+    
+    console.log(newCreature);
+  
+  // safety checks
+    console.log( isCreatureValid(newCreature) );
+    if ( isCreatureValid(newCreature)==false ) { // true==false 
+     return; // stops the function which is "click" one
+    }
 
-); 
+    // add creature to the page
+    addCreatureToDOM(newCreature)
+
+    // save creature to the memory 
+    allCreatures.push(newCreature);
+
+    // reset the fporm prepare for the next iteration
+
+});
